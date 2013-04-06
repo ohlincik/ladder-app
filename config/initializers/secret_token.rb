0f2af55cd3638956f6e6109ea9cb28833616e777
Oto Hlincik <ohlincik@gmail.com>
2013-04-06 11:59:43 -0400

    Create rails app and configure dev db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ladder::Application.config.secret_token = '65e4ec6674d84cdc2ed82099f80410b18a4cef7dc5e155b23366f42d25087192612956333318e06b8054fbde9f8ebe4cbd3e68432dda22ec24cc8d33159c0f40'
