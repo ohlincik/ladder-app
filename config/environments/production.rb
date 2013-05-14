92f32520875132523882b15eb1a4a4f24a410d1b
Oto Hlincik <ohlincik@gmail.com>
2013-05-14 17:17:38 -0400

    Adjust the mailer host to accomodate the renamed heroku app domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Ladder::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'quiet-plateau-5186.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'www.lecomladder.com' }
 end
