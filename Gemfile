3afac8e6001b4767e2f3bec5b7743e48cd6a9ed7
Oto Hlincik <ohlincik@gmail.com>
2014-09-22 20:26:32 -0400

    Upgrade ruby and rails, change seed file syntax

--- Gemfile
+++ Gemfile
@@ -1,38 +1,32 @@
 source 'https://rubygems.org'
 
-# require Ruby 2.0.0 on Heroku
-ruby '2.0.0'
-# require Rails 3.2
-gem 'rails', '~> 4.0.0'
-# HTML, XML, SAX, and Reader parser. Among Nokogiri's many features is the ability to search documents via XPath or CSS3 selectors.
-gem 'nokogiri', '~> 1.6.0'
+ruby '2.1.1'
+gem 'rails', '4.1.6'
 # Thin - A think and fast server
-gem 'thin', '~> 1.6.0'
+gem 'thin', '~> 1.6.2'
 # Postgres Database
-gem 'pg', '~> 0.17.0'
+gem 'pg', '~> 0.17.1'
 # Devise - Flexible authentication solution for Rails with Warden
-gem 'devise', '~> 3.1.1'
+gem 'devise', '~> 3.3.0'
 # Exception notification for Rails apps
 gem 'exception_notification', '~> 4.0.1'
 
 # Gems used only for assets and not required
 # in production environments by default.
-gem 'sass-rails', '~> 4.0.1'
+gem 'sass-rails', '~> 4.0.3'
 gem 'coffee-rails', '~> 4.0.1'
-gem 'uglifier', '~> 2.3.0'
-gem 'asset_sync', '~> 1.0.0'
+gem 'uglifier', '~> 2.5.3'
+gem 'asset_sync', '~> 1.1.0'
 
 # jQuery and the jQuery-ujs driver for your Rails 4 application.
-gem 'jquery-rails', '~> 3.0.4'
+gem 'jquery-rails', '~> 3.1.2'
 # Twitter's Bootstrap, converted to Sass and ready to drop into Rails or Compass
 #gem 'bootstrap-sass', '~>2.3.2.1'
 # Forms made easy
 gem 'simple_form', "~> 3.0.0"
-# provides the capabilities for sorting and reordering a number of objects in a list.
-#gem 'acts_as_list', '0.1.9'
 # Preview email in the browser instead of sending it.
-gem "letter_opener", :group => :development
+gem "letter_opener", group: :development
 # Roadie inlines stylesheets and rewrites relative URLs inside emails.
 gem "roadie", "~> 2.4.2"
 # Draper adds an object-oriented layer of presentation logic
-gem 'draper', '~> 1.3'
+gem 'draper', '~> 1.4.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-#
-# Seed the test players with appropriate ranks
-#
-
 seed_players = [
-	{ :rank => 1, :first_name => "Bob", :last_name => "Uno", :email => "ohlincik+t1@gmail.com", :password => "password", :password_confirmation => "password" },
-	{ :rank => 2, :first_name => "Peter", :last_name => "Deuce", :email => "ohlincik+t2@gmail.com", :password => "password", :password_confirmation => "password" },
-	{ :rank => 3, :first_name => "Frank", :last_name => "Treehorn", :email => "ohlincik+t3@gmail.com", :password => "password", :password_confirmation => "password" },
-	{ :rank => 4, :first_name => "Andy", :last_name => "Foreman", :email => "ohlincik+t4@gmail.com", :password => "password", :password_confirmation => "password" },
-	{ :rank => 5, :first_name => "Chester", :last_name => "Fife", :email => "ohlincik+t5@gmail.com", :password => "password", :password_confirmation => "password" },
-	{ :rank => 6, :first_name => "Jack", :last_name => "Sexton", :email => "ohlincik+t6@gmail.com", :password => "password", :password_confirmation => "password" },
-	{ :rank => 7, :first_name => "Graham", :last_name => "Sieben", :email => "ohlincik+t7@gmail.com", :password => "password", :password_confirmation => "password" },
-	{ :rank => 8, :first_name => "Carl", :last_name => "Octobus", :email => "ohlincik+t8@gmail.com", :password => "password", :password_confirmation => "password" },
-	{ :rank => 9, :first_name => "Hank", :last_name => "Niner", :email => "ohlincik+t9@gmail.com", :password => "password", :password_confirmation => "password" },
-	{ :rank => 10, :first_name => "Paul", :last_name => "Decton", :email => "ohlincik+t10@gmail.com", :password => "password", :password_confirmation => "password" }
+	{ rank: 1, first_name: "Bob", last_name: "Uno", email: "ohlincik+t1@gmail.com", password: "password", password_confirmation: "password" },
+	{ rank: 2, first_name: "Peter", last_name: "Deuce", email: "ohlincik+t2@gmail.com", password: "password", password_confirmation: "password" },
+	{ rank: 3, first_name: "Frank", last_name: "Treehorn", email: "ohlincik+t3@gmail.com", password: "password", password_confirmation: "password" },
+	{ rank: 4, first_name: "Andy", last_name: "Foreman", email: "ohlincik+t4@gmail.com", password: "password", password_confirmation: "password" },
+	{ rank: 5, first_name: "Chester", last_name: "Fife", email: "ohlincik+t5@gmail.com", password: "password", password_confirmation: "password" },
+	{ rank: 6, first_name: "Jack", last_name: "Sexton", email: "ohlincik+t6@gmail.com", password: "password", password_confirmation: "password" },
+	{ rank: 7, first_name: "Graham", last_name: "Sieben", email: "ohlincik+t7@gmail.com", password: "password", password_confirmation: "password" },
+	{ rank: 8, first_name: "Carl", last_name: "Octobus", email: "ohlincik+t8@gmail.com", password: "password", password_confirmation: "password" },
+	{ rank: 9, first_name: "Hank", last_name: "Niner", email: "ohlincik+t9@gmail.com", password: "password", password_confirmation: "password" },
+	{ rank: 10, first_name: "Paul", last_name: "Decton", email: "ohlincik+t10@gmail.com", password: "password", password_confirmation: "password" }
 ]
 
 Player.destroy_all
