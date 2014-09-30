4f6417838cfff10815f60d97f0209d964153306d
Oto Hlincik <ohlincik@gmail.com>
2014-09-30 13:49:19 -0400

    Adjust email addresses in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 seed_players = [
-	{ rank: 1, first_name: "Bob", last_name: "Uno", email: "ohlincik+t1@gmail.com", password: "password", password_confirmation: "password" },
-	{ rank: 2, first_name: "Peter", last_name: "Deuce", email: "ohlincik+t2@gmail.com", password: "password", password_confirmation: "password" },
-	{ rank: 3, first_name: "Frank", last_name: "Treehorn", email: "ohlincik+t3@gmail.com", password: "password", password_confirmation: "password" },
-	{ rank: 4, first_name: "Andy", last_name: "Foreman", email: "ohlincik+t4@gmail.com", password: "password", password_confirmation: "password" },
-	{ rank: 5, first_name: "Chester", last_name: "Fife", email: "ohlincik+t5@gmail.com", password: "password", password_confirmation: "password" },
-	{ rank: 6, first_name: "Jack", last_name: "Sexton", email: "ohlincik+t6@gmail.com", password: "password", password_confirmation: "password" },
-	{ rank: 7, first_name: "Graham", last_name: "Sieben", email: "ohlincik+t7@gmail.com", password: "password", password_confirmation: "password" },
-	{ rank: 8, first_name: "Carl", last_name: "Octobus", email: "ohlincik+t8@gmail.com", password: "password", password_confirmation: "password" },
-	{ rank: 9, first_name: "Hank", last_name: "Niner", email: "ohlincik+t9@gmail.com", password: "password", password_confirmation: "password" },
-	{ rank: 10, first_name: "Paul", last_name: "Decton", email: "ohlincik+t10@gmail.com", password: "password", password_confirmation: "password" }
+	{ rank: 1, first_name: "Bob", last_name: "Uno", email: "1@ladder.com", password: "password", password_confirmation: "password" },
+	{ rank: 2, first_name: "Peter", last_name: "Deuce", email: "2@ladder.com", password: "password", password_confirmation: "password" },
+	{ rank: 3, first_name: "Frank", last_name: "Treehorn", email: "3@ladder.com", password: "password", password_confirmation: "password" },
+	{ rank: 4, first_name: "Andy", last_name: "Foreman", email: "4@ladder.com", password: "password", password_confirmation: "password" },
+	{ rank: 5, first_name: "Chester", last_name: "Fife", email: "5@ladder.com", password: "password", password_confirmation: "password" },
+	{ rank: 6, first_name: "Jack", last_name: "Sexton", email: "6@ladder.com", password: "password", password_confirmation: "password" },
+	{ rank: 7, first_name: "Graham", last_name: "Sieben", email: "7@ladder.com", password: "password", password_confirmation: "password" },
+	{ rank: 8, first_name: "Carl", last_name: "Octobus", email: "8@ladder.com", password: "password", password_confirmation: "password" },
+	{ rank: 9, first_name: "Hank", last_name: "Niner", email: "9@ladder.com", password: "password", password_confirmation: "password" },
+	{ rank: 10, first_name: "Paul", last_name: "Decton", email: "10@ladder.com", password: "password", password_confirmation: "password" }
 ]
 
 Player.destroy_all
